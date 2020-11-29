deff4d0646c8e95c686edd9acef727299c21a00a
Priscila <paraujocaimi@hotmail.com>
2020-11-29 15:47:45 -0300

    alterando numero de a e z no preview de elementos

--- Assets/Scripts/PreviewNextElement.cs
+++ Assets/Scripts/PreviewNextElement.cs
@@ -19,31 +19,58 @@ public class PreviewNextElement : MonoBehaviour
     public Text z_beta;
     public Text element_beta;
 
-    public void updateAlfaHud(int a, int z, string name)
+    private void updateAlfaHud(int a, int z, string name)
     {
         a_alfa.text = a.ToString();
         z_alfa.text = z.ToString();
         element_alfa.text = name;
     }
 
-    public void updateNeutronHud(int a, int z, string name)
+    private void updateNeutronHud(int a, int z, string name)
     {
         a_neutron.text = a.ToString();
         z_neutron.text = z.ToString();
         element_neutron.text = name;
     }
 
-    public void updateCapturaEletronicaHud(int a, int z, string name)
+    private void updateCapturaEletronicaHud(int a, int z, string name)
     {
         a_captura.text = a.ToString();
         z_captura.text = z.ToString();
         element_captura.text = name;
     }
 
-    public void updateBetaHud(int a, int z, string name)
+    private void updateBetaHud(int a, int z, string name)
     {
         a_beta.text = a.ToString();
         z_beta.text = z.ToString();
         element_beta.text = name;
+    }
+
+    public void update(string tipoemissao, int a, int z, string name)
+    {
+        string emissao = tipoemissao;
+
+        switch (emissao)
+        {
+            case "alfa":
+                a += -4;
+                z += -2;
+                updateAlfaHud(a, z, name);
+                break;
+            case "beta":
+                z += +1;
+                updateBetaHud(a, z, name);
+                break;
+            case "ec":
+                z += -1;
+                updateCapturaEletronicaHud(a, z, name);
+                break;
+            case "neutron":
+                a += 1;
+                updateNeutronHud(a, z, name);
+                break;
+        }
+
     }
 }
