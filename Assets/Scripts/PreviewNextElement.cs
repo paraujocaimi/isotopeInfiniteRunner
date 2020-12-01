cc34180feab3b681fa23f40962456717d9258782
Priscila <paraujocaimi@hotmail.com>
2020-11-30 22:07:40 -0300

    preview do próximo elemento
    - implementado frontend
    - implementado backend
    - alteração da cor se não existir o isotopo

--- Assets/Scripts/PreviewNextElement.cs
+++ Assets/Scripts/PreviewNextElement.cs
@@ -25,6 +25,14 @@ public class PreviewNextElement : MonoBehaviour
         a_alfa.text = a.ToString();
         z_alfa.text = z.ToString();
         element_alfa.text = name;
+
+        if (exists.Equals(false))
+        {
+            a_alfa.text = "<color=#A93226>" + a_alfa.text + "</color>";
+            z_alfa.text = "<color=#A93226>" + z_alfa.text + "</color>";
+            element_alfa.text = "<color=#A93226>" + element_alfa.text + "</color>";
+        }
+
     }
 
     public void updateNeutronHud(int a, int z, string name, bool exists)
@@ -32,6 +40,13 @@ public class PreviewNextElement : MonoBehaviour
         a_neutron.text = a.ToString();
         z_neutron.text = z.ToString();
         element_neutron.text = name;
+
+        if (exists.Equals(false))
+        {
+            a_neutron.text = "<color=#A93226>" + a_neutron.text + "</color>";
+            z_neutron.text = "<color=#A93226>" + z_neutron.text + "</color>";
+            element_neutron.text = "<color=#A93226>" + element_neutron.text + "</color>";
+        }
     }
 
     public void updateCapturaEletronicaHud(int a, int z, string name, bool exists)
@@ -39,6 +54,13 @@ public class PreviewNextElement : MonoBehaviour
         a_captura.text = a.ToString();
         z_captura.text = z.ToString();
         element_captura.text = name;
+
+        if (exists.Equals(false))
+        {
+            a_captura.text = "<color=#A93226>" + a_captura.text + "</color>";
+            z_captura.text = "<color=#A93226>" + z_captura.text + "</color>";
+            element_captura.text = "<color=#A93226>" + element_captura.text + "</color>";
+        }
     }
 
     public void updateBetaHud(int a, int z, string name, bool exists)
@@ -46,6 +68,13 @@ public class PreviewNextElement : MonoBehaviour
         a_beta.text = a.ToString();
         z_beta.text = z.ToString();
         element_beta.text = name;
+
+        if (exists.Equals(false))
+        {
+            a_beta.text = "<color=#A93226>" + a_beta.text + "</color>";
+            z_beta.text = "<color=#A93226>" + z_beta.text + "</color>";
+            element_beta.text = "<color=#A93226>" + element_beta.text + "</color>";
+        }
     }
 
 }
